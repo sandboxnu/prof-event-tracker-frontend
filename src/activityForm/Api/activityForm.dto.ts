c8ebe6f9f9ab0a507437a8db69942860564a0859
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2023-01-22 21:44:45 -0500

    Create the form page (#9)
    * Activity Form Page

---
+++ src/activityForm/Api/activityForm.dto.ts
@@ -0,0 +1,16 @@
+import { ActivityCategory, ActivityWeight } from "../form.store";
+
+export type Semester = 'Fall' | 'Spring' | 'Summer 1' | 'Summer 2';
+
+export type CreateActivityDto = {
+    userId: number;
+    academicYearId: number;
+    year: number;
+    semester: Semester;
+    date?: Date;
+    name: string;
+    description: string;
+    category: ActivityCategory;
+    significance: ActivityWeight;
+    isFavorite: boolean;
+};
